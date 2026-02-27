f31d4a28acbdc3a03ebe1fc642a66b9b91d6dde3
Frederic <afredof@gmail.com>
2026-02-27 16:26:14 +0100

    fix: export nommé prisma pour compatibilité build

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -8,8 +8,10 @@ declare global {
     var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>
 }
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+// On change "const prisma" par "export const prisma"
+export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
 
+// On garde l'export default par sécurité si certains fichiers l'utilisent
 export default prisma
 
 if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
